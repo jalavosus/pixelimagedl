0b7ea8ec6aaa3d0f54010a2c76b6b7212143dc49
James Alavosus <alavosus.james@gmail.com>
2022-05-11 14:53:31 -0400

    Bump pp version to v3

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
-	github.com/k0kubun/pp v2.4.0+incompatible
+	github.com/k0kubun/pp/v3 v3.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/schollz/progressbar/v3 v3.8.6
 	github.com/urfave/cli/v2 v2.6.0
@@ -13,8 +13,7 @@ require (
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
@@ -24,4 +23,5 @@ require (
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- pkg/pixelimagedl/types.go
+++ pkg/pixelimagedl/types.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/k0kubun/pp"
+	"github.com/k0kubun/pp/v3"
 )
 
 func init() {
@@ -18,17 +18,20 @@ func init() {
 }
 
 type PixelImage struct {
-	Version      string `json:"version" yaml:"version" pp:"-"`
-	BuildNumber  string `json:"build_number" yaml:"build_number" pp:"-"`
-	BuildDate    string `json:"build_date" yaml:"build_date" pp:"-"`
-	BuildComment string `json:"build_comment,omitempty" yaml:"build_comment,omitempty"`
-	DownloadURI  string `json:"download_uri" yaml:"download_uri" pp:"-"`
-	SHA256Sum    string `json:"sha256_sum" yaml:"sha256_sum" pp:"-"`
+	Version      string `json:"version" yaml:"version" pp:"version"`
+	BuildNumber  string `json:"build_number" yaml:"build_number" pp:"build_number"`
+	BuildDate    string `json:"build_date" yaml:"build_date" pp:"build_date"`
+	BuildComment string `json:"build_comment,omitempty" yaml:"build_comment,omitempty" pp:"build_comment,omitempty"`
+	DownloadURI  string `json:"download_uri" yaml:"download_uri" pp:"download_uri"`
+	SHA256Sum    string `json:"sha256_sum" yaml:"sha256_sum" pp:"sha256_sum"`
 }
 
 func (f PixelImage) PrettyPrint() {
+	out := new(bytes.Buffer)
+	_, _ = pp.Fprint(out, f)
+
 	fmt.Println(string(bytes.Replace(
-		[]byte(pp.Sprint(f)),
+		out.Bytes(),
 		[]byte("pixelimagedl.\x1b[32mPixelImage\x1b[0m"),
 		[]byte(""),
 		1,
